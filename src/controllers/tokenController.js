c186c9b7465146b8b2739c78302c841dd17044d2
Matheus Ribeiro <matheus.ribeiro@amigoapp.com.br>
2020-05-25 22:29:52 -0300

    Jwt inserted into the routes.

--- src/controllers/tokenController.js
+++ src/controllers/tokenController.js
@@ -5,24 +5,6 @@ const async = require('async')
 
 module.exports = () => {
     const store = (req, res) => {
-        // const userModel = new User(req.body)
-        // const { email } = req.body;
-        // let password = req.body.password;
-        // const id = req.params.user_id;
-        // const user =  await User.findOne({ where: { id, email }, raw: true});
-
-        // if (!email || !user || !password) return res.status(401).json({Error: "The email and password are both required!"})
-
-        // password = await userModel.comparePassword(password, user.password)
-
-        // if(!password) return res.status(401).json({Error: 'Password informed is not valid!'})
-
-        // const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
-        //     expiresIn: process.env.TOKEN_EXPIRATION
-        // })
-
-        // res.json({token});
-
         const userModel = new User(req.body);
         const { email } = req.body;
         let password = req.body.password;
@@ -73,10 +55,8 @@ module.exports = () => {
                 return res.json({token})
             }]
         })
-    }
+    };
     
-
-
     return {
         store
     }
